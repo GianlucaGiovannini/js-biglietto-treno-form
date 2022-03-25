f5121b0afd46f8a7fc7c0accaf3dd414be6057f8
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-03-25 18:43:39 +0100

    Code Review

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,23 @@
 // chiedere il nome all'utente 
 function generate() {
 
-
-
+    // nome
     let userName = document.getElementById("nome").value;
     document.getElementById("stamp_nome").innerHTML = userName;
 
+    // km percorsi
     let kmPercorso = document.getElementById("km_percorso").value;
     document.getElementById("stamp_km").innerHTML = kmPercorso;
 
+    // età
     let userEta = document.getElementById("fascia_eta").value;
 
-    const prezzoKm = 0.21 // prezzo al km
+    // prezzo al km
+    const prezzoKm = 0.21
     const prezzoUtente = prezzoKm * kmPercorso;
     const prezzoMinori = prezzoUtente * 0.8;
     const prezzoOver_65 = prezzoUtente * 0.6;
-
-    console.log(prezzoKm, prezzoUtente, prezzoMinori, prezzoOver_65)
-
+    // stampa 
     if (userEta === "minorenne") {
         document.getElementById("tipo_offerta").innerHTML = "Sconto Minorenne";
         document.getElementById("price_ticket").innerHTML = "€ " + prezzoMinori.toFixed(2)
@@ -28,15 +28,11 @@ function generate() {
         document.getElementById("tipo_offerta").innerHTML = "Nessuna offerta";
         document.getElementById("price_ticket").innerHTML = "€ " + prezzoUtente.toFixed(2)
     }
-
+    // carrozza 
     const carrozza = Math.floor(Math.random() * 11);
     document.getElementById("n_carrozza").innerHTML = carrozza;
-
+    // codice cp 
     const codiceCP = Math.floor(Math.random() * 100000);
     document.getElementById("n_cp").innerHTML = codiceCP;
-
-
-
-
 
 }
