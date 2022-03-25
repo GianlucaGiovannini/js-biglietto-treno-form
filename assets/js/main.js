ffbb2b4890d20578be118614098312cfd58943cf
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-03-25 20:45:43 +0100

    Code Review

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,6 @@ function generate() {
     // nome
     let userName = document.getElementById("nome").value;
     document.getElementById("stamp_nome").innerHTML = userName;
-
     // km percorsi
     let kmPercorso = document.getElementById("km_percorso").value;
 
@@ -16,6 +15,7 @@ function generate() {
     const prezzoUtente = prezzoKm * kmPercorso;
     const prezzoMinori = prezzoUtente * 0.8;
     const prezzoOver_65 = prezzoUtente * 0.6;
+
     // stampa 
     if (userEta === "minorenne") {
         document.getElementById("tipo_offerta").innerHTML = "Sconto Minorenne";
@@ -27,9 +27,11 @@ function generate() {
         document.getElementById("tipo_offerta").innerHTML = "Nessuna offerta";
         document.getElementById("price_ticket").innerHTML = "€ " + prezzoUtente.toFixed(2)
     }
+
     // carrozza 
     const carrozza = Math.floor(Math.random() * 11);
     document.getElementById("n_carrozza").innerHTML = carrozza;
+
     // codice cp 
     const codiceCP = Math.floor(Math.random() * 100000);
     document.getElementById("n_cp").innerHTML = codiceCP;
